24a4de95861b6f78055fef540e40b1145cf632e3
Nikitin-Sasha-apcsa-fall2022 <nikitina8580@my.sduhsd.net>
2022-09-12 16:25:37 -0700

    triples update

--- unit-07/src/Triples.java
+++ unit-07/src/Triples.java
@@ -26,38 +26,36 @@ public class Triples
 
 	}
 	
-	private int greatestCommonFactor(int a, int b, int c)
+	private boolean greatestCommonFactor(int a, int b, int c)
 	{
-		for (int i=a/2+1; i>0; i--)
-			if (a%i == 0)
-				for (int j=b/2+1;j>0;j--)
-					if(b%j== 0)
-						for (int k =c/2+1; k>0; k--)
-							if(c%k ==0)
-								if (i==1 && i==j && i==k)
-									return 1;
-		return 0;
-		
-		
+		int max = 0;
+		for(int i = 1; i<=a || i<=b || i<=c; i++) {
+			if(a%i==0 && b%i == 0 && c%i ==0) {
+				max = i;
+			}
+		}
+
+
+		return max == 1;
 	}
 
 	public String toString()
 	{
 		String output="";
-for (int a =1; a<=number; a++)
-	for(int b =1; b<=number; b++)
-		for( int c=1; c<=number; c++)
-			if(a*a + b*b == c*c)
-					if((a%2==1 && b%2==0) || (a%2==0 && b%2==1))
-						if(c%2==1)
-							if(greatestCommonFactor(a,b,c)==1)
-								if(a<b)
-									output += a + " " + b + " " + c + "\n"; 
+
+		for(int a =1; a<=number; a++) {
+			for(int b = a+1; b<=number; b++) {
+				for(int c = b+1; c<=number; c++) {
+					if(Math.pow(a, 2)+Math.pow(b, 2)==Math.pow(c, 2) && greatestCommonFactor(a,b,c)) {
+						output+= a + " " + b + " " + c + "\n";;
+					}
+				}
+			}
+		}
 
 
 
 
-
-		return output+"\n";
+		return output+ "\n";
 	}
 }
