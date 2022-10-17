9db0fb62eb1a7963c3cf0ba759ad7c74b533c257
Nikitin-Sasha-apcsa-fall2022 <nikitina8580@my.sduhsd.net>
2022-10-17 15:47:48 -0700

    unit 12 done

---
+++ unit-12/src/MonsterRunner.java
@@ -0,0 +1,48 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class MonsterRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		//ask for name and size
+		System.out.println("Enter 1st monster's name : ");
+		String name = keyboard.next();
+		System.out.println("Enter 1st monster's size : ");
+		int size = keyboard.nextInt(); 
+		//instantiate monster one
+		Skeleton monster1 = new Skeleton(name,size); 
+		
+		//ask for name and size
+		System.out.println("Enter 2st monster's name : ");
+		String name1 = keyboard.next();
+		System.out.println("Enter 2st monster's size : ");
+		int size2 = keyboard.nextInt(); 
+		//instantiate monster two
+		Skeleton monster2 = new Skeleton(name1,size2); 
+		
+		System.out.println("Monster 1 - " + monster1);
+		System.out.println("Monster 2 - " + monster2); 
+		
+		if(monster1.isBigger(monster2)) {
+			System.out.println("Monster one is bigger than Monster two.");
+		}
+		else {
+			System.out.println("Monster one is smaller than Monster two.");
+		}
+		
+		if(monster1.namesTheSame(monster2)) {
+			System.out.println("Monster one has the same name as Monster two.");
+		}
+		else {
+			System.out.println("Monster one does not have the same name as Monster two.");
+		}
+		
+	}
+}
