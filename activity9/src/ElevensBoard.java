c448c5714775641c4d8cbea85808c12f525a7a4d
Nikitin-Sasha-apcsa-fall2022 <nikitina8580@my.sduhsd.net>
2022-10-24 10:27:45 -0700

    9

--- activity9/src/ElevensBoard.java
+++ activity9/src/ElevensBoard.java
@@ -54,6 +54,11 @@ public class ElevensBoard extends Board {
 	@Override
 	public boolean isLegal(List<Integer> selectedCards) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+		if(selectedCards.size()==2) 
+			return containsPairSum11(selectedCards);
+		if(selectedCards.size()==3)
+			return containsJQK(selectedCards);
+		return false;
 	}
 
 	/**
@@ -67,6 +72,10 @@ public class ElevensBoard extends Board {
 	@Override
 	public boolean anotherPlayIsPossible() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+		ArrayList<Integer> the = (ArrayList<Integer>) cardIndexes();
+		if(containsPairSum11(the) || containsJQK(the)) return true;
+		return false;
+		
 	}
 
 	/**
@@ -79,6 +88,14 @@ public class ElevensBoard extends Board {
 	 */
 	private boolean containsPairSum11(List<Integer> selectedCards) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+		if(selectedCards.size() < 2) return false;
+		for(int i = 0; i < selectedCards.size(); i++) {
+			for(int j = 0; j<selectedCards.size(); j++) {
+				if(cardAt(selectedCards.get(i)).pointValue() + cardAt(selectedCards.get(j)).pointValue() == 11) 
+					return true;
+			}
+		}
+		return false;
 	}
 
 	/**
@@ -91,5 +108,17 @@ public class ElevensBoard extends Board {
 	 */
 	private boolean containsJQK(List<Integer> selectedCards) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
+		boolean containsJack = false;
+		boolean containsQueen = false;
+		boolean containsKing = false;
+		for(int i = 0; i< selectedCards.size(); i++) {
+			if(cardAt(selectedCards.get(i)).rank() == "jack")
+				containsJack = true;
+			if(cardAt(selectedCards.get(i)).rank() == "queen")
+				containsQueen = true;
+			if(cardAt(selectedCards.get(i)).rank() == "king")
+				containsKing= true;
+		}
+		return containsJack && containsQueen && containsKing;
 	}
 }
